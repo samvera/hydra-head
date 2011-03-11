5ceb15cb892f3196f1c7d5ab2a81c9e9bb1ce016
Matt Zumwalt <matt.zumwalt@yourmediashelf.com>
2011-03-11 00:34:59 -0600

    adding rspec, removing unwanted files

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-HydraRepository::Application.config.secret_token = 'ae08d2388bc457c7f4eff2b4729e86652694cee6b8164d8a91048af79312757a01f8460da5d1ac055a7d53cd5509e17cacb7b9b95ff0627df15934904da2b7f5'
