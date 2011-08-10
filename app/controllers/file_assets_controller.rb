58cca721a8eaefbbbb57150a18dc5a04ba16e607
Jessie Keck <jessie.keck@gmail.com>
2011-08-10 11:21:15 -0700

    Moving module include statment after requiring solr/fedora.

--- app/controllers/file_assets_controller.rb
+++ app/controllers/file_assets_controller.rb
@@ -5,10 +5,14 @@ class FileAssetsController < ApplicationController
   include Hydra::RepositoryController  
   include MediaShelf::ActiveFedoraHelper
   include Blacklight::SolrHelper
-  include Hydra::SubmissionWorkflow
+  
   
   before_filter :require_fedora
   before_filter :require_solr, :only=>[:index, :create, :show, :destroy]
+
+  # need to include this after the :require_solr/fedora before filters because of the before filter that the workflow provides.
+  include Hydra::SubmissionWorkflow
+
   prepend_before_filter :sanitize_update_params
   
   def index
