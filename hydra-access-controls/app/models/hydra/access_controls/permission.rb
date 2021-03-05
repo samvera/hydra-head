e8d4f6bd4f52f006098b377b6109c50fe3145050
Max Kadel <max@curationexperts.com>
2021-03-05 11:26:10 -0500

    Back to ground state to see what CI looks like

--- hydra-access-controls/app/models/hydra/access_controls/permission.rb
+++ hydra-access-controls/app/models/hydra/access_controls/permission.rb
@@ -41,7 +41,7 @@ module Hydra::AccessControls
     end
 
     def agent_name
-      URI.decode_www_form(parsed_agent.last)
+      URI.decode(parsed_agent.last)
     end
 
     def update(*)
@@ -80,7 +80,7 @@ module Hydra::AccessControls
     end
 
     def build_agent_resource(prefix, name)
-      [Agent.new(::RDF::URI.new("#{prefix}##{URI.encode_www_form_component(name)}"))]
+      [Agent.new(::RDF::URI.new("#{prefix}##{URI.encode(name)}"))]
     end
 
     def build_access(access)
