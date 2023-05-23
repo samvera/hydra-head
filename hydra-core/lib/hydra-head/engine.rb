8fb71af74bb81dc3603aec615bfb7ac8bf82e4ae
Chris Colvard <chris.colvard@gmail.com>
2023-05-23 14:40:44 -0400

    Remove autoload that is unneeded in zeitwerk mode

--- hydra-core/lib/hydra-head/engine.rb
+++ hydra-core/lib/hydra-head/engine.rb
@@ -4,11 +4,6 @@ module HydraHead
     # Config defaults
     config.mount_at = '/'
 
-    config.autoload_paths += %W(
-      #{config.root}/app/controllers/concerns
-      #{config.root}/app/models/concerns
-    )
-    
     # Load rake tasks
     rake_tasks do
       Dir.glob(File.join(File.expand_path('../', File.dirname(__FILE__)),'railties', '*.rake')).each do |railtie|
